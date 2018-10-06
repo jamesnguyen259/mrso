5eb7f0c641a903ec520de213b1fef873b70e8f36
Admin <admin@Ebolaa-2.local>
2018-10-06 15:23:49 +0700

    add cloundinary and film controller
    ed

---
+++ app/controllers/films_controller.rb
@@ -0,0 +1,18 @@
+class FilmsController < ApplicationController
+  def new
+  	@film = Film.new
+  end
+
+  def create
+  	@film = Film.new film_params
+  	unless @film.save
+  		render :new
+  	end
+  end
+
+  private
+
+  def film_params
+    params.require(:film).permit Film::ATTRIBUTES_PARAMS
+  end
+end
