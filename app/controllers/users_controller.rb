73009cbc68d5fdb7773871984a23bd3abb51a3db
canhchimmcodon <ndhoang.bk.hedspi@gmail.com>
2018-10-21 23:05:59 +0700

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,6 @@
 class UsersController < ApplicationController
-
-    def show
-      @user = User.find(params[:id])
-    end
-  
     def new
       @user = User.new
-    end
-  
-    def edit
-      @user = User.find(params[:id])
     end
 
     def create
@@ -22,21 +13,28 @@ class UsersController < ApplicationController
           render 'new'
         end
     end
-  
-    def update
-      byebug
-        @user = User.find(params[:id])
-        if @user.update_attributes(user_params)
-            flash[:success] = "Profile updated"
-              redirect_to @user
-          # Handle a successful update.
-        else
-          render 'edit'
-        end
+
+    def edit
+      @user = User.find(params[:id])
     end
-  
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+          flash[:success] = "Profile updated"
+            redirect_to @user
+        # Handle a successful update.
+      else
+        render 'edit'
+      end
+    end
+
     private
-  
+
       def user_params
         params.require(:user).permit(:name, :email, :password,
                                      :password_confirmation)
